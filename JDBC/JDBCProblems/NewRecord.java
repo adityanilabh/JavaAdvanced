7c110d935d479343bc95603a81f101329b5b41b2
adityanilabh <nilabhaditya6@gmail.com>
2024-02-16 16:06:41 +0530

    commit made jdbc

---
+++ JDBC/JDBCProblems/NewRecord.java
@@ -0,0 +1,14 @@
+package JDBCProblems;
+
+public class NewRecord {
+    public static void main(String[] args)
+    {
+        try{
+            
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
